4a3633a653b61daf411a2d7957fe1e2be20f365a
平林真奈 <>
2024-05-15 19:06:32 +0900

    supabaseで認証機能実装用にクローン

---
+++ src/app/_components/Header/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function page() {
+  return (
+    <header className="sticky flex text-white justify-center border-b">
+      <div className="flex items-center justify-end w-full h-16 max-w-3xl px-4 mx-auto sm:px-6">
+ログインボタン入る
+      </div>
+    </header>
+  );
+}
